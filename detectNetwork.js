6291ba391c949007ed5f3cb2b25b236cc32e2768
Calin Lewis <calin.lewis@gmail.com>
2018-05-02 21:54:15 -0700

    Release version 1.0: Diner's Club and Amex
    Supports credit cards in "Diner's Club" and "American Express"
    formats.

--- detectNetwork.js
+++ detectNetwork.js
@@ -7,12 +7,37 @@
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
 
-var detectNetwork = function(cardNumber) {
+/* 
+
+Function Notes: 
   // Note: `cardNumber` will always be a string
+
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
-
+     Visa always has a prefix of 4 and a length of 13, 16, or 19.
+     MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+
+Execution: 
+
+Example string: '343456789012345'
+
+1) Store string length in variable.  
+2) Store first two characters of string in variable, convert to number
+3) If string length is 14 and first two characters = 38 OR 39, return 'Diner's Club', othwerwise:
+4) If string length is 15 and first two characters = 34 or 37, return 'American Express';
+
+*/
+
+
+var detectNetwork = function(cardNumber) {
+
+  var cardNumberLength = cardNumber.length; 
+  var cardNumberPrefix = parseInt(cardNumber[0] + cardNumber[1]);
+  if((cardNumberLength === 14) && (cardNumberPrefix === 38 || 39)){
+  	return 'Diner\'s Club';
+  } else if ((cardNumberLength === 15) && (cardNumberPrefix === 34 || 37)){
+  	return 'American Express';
+  }
+
 };
-
-
