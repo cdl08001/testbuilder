0774245233273c27b17b80b208bd240844ddd5f5
Calin Lewis <calin.lewis@gmail.com>
2018-05-02 22:33:28 -0700

    Support Visa and MasterCard. Enhance Logic.
    Added support for visa and mastercard number entries.
    Modified conditional statements for readability.

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,22 +1,24 @@
-// Given a credit card number, this function should return a string with the 
-// name of a network, like 'MasterCard' or 'American Express'
-// Example: detectNetwork('343456789012345') should return 'American Express'
-
-// How can you tell one card network from another? Easy! 
-// There are two indicators:
-//   1. The first few numbers (called the prefix)
-//   2. The number of digits in the number (called the length)
-
 /* 
+Given a credit card number, this function should return a string with the 
+name of a network, like 'MasterCard' or 'American Express'
+Example: detectNetwork('343456789012345') should return 'American Express'
+
+How can you tell one card network from another? Easy! 
+  There are two indicators:
+   1. The first few numbers (called the prefix)
+   2. The number of digits in the number (called the length)
+
+
 
 Function Notes: 
-  // Note: `cardNumber` will always be a string
+  Note: `cardNumber` will always be a string
 
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
-     Visa always has a prefix of 4 and a length of 13, 16, or 19.
-     MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
+   Diner's Club network always starts with a 38 or 39 and is 14 digits long
+   American Express network always starts with a 34 or 37 and is 15 digits long
+   Visa always has a prefix of 4 and a length of 13, 16, or 19.
+   MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
+   
+  Once you've read this, go ahead and try to implement this function, then return to the console.
 
 Execution: 
 
@@ -29,15 +31,25 @@ Example string: '343456789012345'
 
 */
 
-
 var detectNetwork = function(cardNumber) {
-
   var cardNumberLength = cardNumber.length; 
-  var cardNumberPrefix = parseInt(cardNumber[0] + cardNumber[1]);
-  if((cardNumberLength === 14) && (cardNumberPrefix === 38 || 39)){
-  	return 'Diner\'s Club';
-  } else if ((cardNumberLength === 15) && (cardNumberPrefix === 34 || 37)){
-  	return 'American Express';
-  }
+  var cardNumberPrefix = cardNumber[0] + cardNumber[1];
 
-};
+  if(cardNumberLength === 14){
+  	if(cardNumberPrefix === '38' || '39'){
+  	  return 'Diner\'s Club';
+  	}
+  } else if(cardNumberLength === 15){
+  	  if(cardNumberPrefix === '34' || '37'){
+  	    return 'American Express';
+  	  }
+  } else if(cardNumberLength === 13 || 16 || 19){
+      if(cardNumberPrefix[0] === '4'){
+      	return 'Visa';
+      } else if(cardNumberLength === 16){
+          if(cardNumberPrefix === '51' || '52' || '53' || '54' || '55'){
+      	    return 'MasterCard';
+          }
+      }
+  }
+};  
