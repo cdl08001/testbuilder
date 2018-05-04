839c15e60ede0a3d3e54900f1015b5e7fff1b962
Calin Lewis <calin.lewis@gmail.com>
2018-05-04 13:08:23 -0700

    Update Function Notes
    Updated function notes for readabilty.

--- detectNetwork.js
+++ detectNetwork.js
@@ -8,22 +8,34 @@ How can you tell one card network from another? Easy!
    1. The first few numbers (called the prefix)
    2. The number of digits in the number (called the length)
 
-
-
 Function Notes: 
-  Note: `cardNumber` will always be a string
+   Note: `cardNumber` = string
 
-   Diner's Club network always starts with a 38 or 39 and is 14 digits long
-   American Express network always starts with a 34 or 37 and is 15 digits long
-   Visa always has a prefix of 4 and a length of 13, 16, or 19.
-   MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
-   Discover always has a prefix of 6011, 644-649, or 65, and a length of 16 or 19.
-   Maestro always has a prefix of 5018, 5020, 5038, or 6304, and a length of 12-19.
+   Diner's Club Prefix: 38, 39 
+   Diner's Club Length: 14
 
-   China UnionPay always has a prefix of 622126-622925, 624-626, or 6282-6288 and a length of 16-19.
-   Switch always has a prefix of 4903, 4905, 4911, 4936, 564182, 633110, 6333, or 6759 and a length of 16, 18, or 19.
+   American Express Prefix: 34, 37
+   American Express Length: 15
 
-Heads up! Switch and Visa seem to have some overlapping card numbers - in any apparent conflict, you should choose the network with the longer prefix.
+   Visa Prefix: 4
+   Visa Length: 13, 16, 19
+
+   MasterCard Prefix: 51, 52, 53, 54, 55
+   MasterCard Length: 16
+
+   Discover Prefix: 6011, 644-649, 65
+   Discover Length: 16, 19
+
+   Maestro Prefix: 5018, 5020, 5038, 6304
+   Maestro Length: 12-19
+
+   China UnionPay Prefix: 622126-622925, 624-626,6282-6288 
+   China UnionPay Length: 16-19
+
+   Switch Prefix: 4903, 4905, 4911, 4936, 564182, 633110, 6333, 6759
+   Switch Length: 16, 18, or 19
+
+   Heads up! Switch and Visa seem to have some overlapping card numbers - in any apparent conflict, you should choose the network with the longer prefix.
 
   Once you've read this, go ahead and try to implement this function, then return to the console.
 
@@ -86,8 +98,11 @@ var detectNetwork = function(cardNumber) {
   	  if(cardNumberLength === 15){
   	    return 'American Express';
   	  }
-  	  	  
-  } else if(visaPrefixes.includes(firstCardDigit)){
+
+  } else if(switchPayPrefixes.includes())
+
+
+  else if(visaPrefixes.includes(firstCardDigit)){
       if(cardNumberLength === 13 || 16 || 19){
         return 'Visa';
       }
